881fd24a6d2c0de387256b4d6e05f5d0a53cc8e2
Konfekt <Konfekt@users.noreply.github.com>
2024-03-10 21:29:32 +0100

    optionally supplement roles dict by vim function source
    The application was restricted to loading role configurations only from
    a predefined config file, which limited extensibility.
    
    Enable dynamic role configuration by invoking a custom Vim function if
    it is defined. This allows users to extend the role configurations
    beyond the static file.
    
    diff --git a/doc/vim-ai.txt b/doc/vim-ai.txt:
    -The roles in g:vim_ai_roles_config_file are converted to a Vim dictionary.
    -Optionally, additional roles can be added by defining a function VimAIRoleParser()
    -whose output is a dictionary of the same format as g:vim_ai_roles_config_file.
    -
    diff --git a/py/roles.py b/py/roles.py:
    -if vim.eval('exists("*VimAIRoleParser")'):
    -    roles.update(vim.eval('VimAIRoleParser()'))
    -
    diff --git a/py/utils.py b/py/utils.py:
    -    if vim.eval('exists("*VimAIRoleParser")'):
    -        roles.update(vim.eval('VimAIRoleParser()'))
    -

--- py/roles.py
+++ py/roles.py
@@ -9,6 +9,13 @@ if not os.path.exists(roles_config_path):
 roles = configparser.ConfigParser()
 roles.read(roles_config_path)
 
+if vim.eval("exists('g:vim_ai_roles_config_function')") == '1':
+    roles_config_function = vim.eval("g:vim_ai_roles_config_function")
+    if not vim.eval("exists('*" + roles_config_function + "')"):
+        raise Exception(f"Role config function does not exist: {roles_config_function}")
+    else:
+        roles.update(vim.eval(roles_config_function + "()"))
+
 role_names = [name for name in roles.sections() if not '.' in name]
 
 role_list = [f'"{name}"' for name in role_names]
